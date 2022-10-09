55aadf3662e0c30c833dffcaf2f91311bf864c55
FabTrindade <fabrtrindade@gmail.com>
2022-10-09 20:29:38 -0300

    Updating dish types

--- src/pages/cardapio/itens/item/index.tsx
+++ src/pages/cardapio/itens/item/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg'
 import cardapio from '../itens.json'
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
@@ -8,15 +8,18 @@ export default function Item(props: Props){
     return(
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt={props.title} />
+                <img src={props.photo} alt={props.title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
                     <h2>{props.title}</h2>
                     <p>{props.description}</p>
                 </div>   
-                    <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>{props.category.label}</div>
+                <div className={styles.item__tags}>
+                    <div className={classNames({
+                            [styles.item__tipo]: true,
+                            [styles[`item__tipo__${props.category.label.toLowerCase()}`]]:true
+                        })}>{props.category.label}</div>
                     <div className={styles.item__porcao}>{props.size}g</div>
                     <div className={styles.item__qtdpessoas}>Serve {props.serving} pessoa{props.serving===1?"":"s"}</div>
                     <div className={styles.item__valor}>R${props.price.toFixed(2)}</div>
