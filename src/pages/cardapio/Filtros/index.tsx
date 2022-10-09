e31a22f22574bd638b8a3d3d1f5079da673c77ab
FabTrindade <fabrtrindade@gmail.com>
2022-10-08 21:25:04 -0300

    Making filters work

--- src/pages/cardapio/Filtros/index.tsx
+++ src/pages/cardapio/Filtros/index.tsx
@@ -1,17 +1,34 @@
-import filtros from './filtros.json'
-import styles from './Filtros.module.scss'
+import { Dispatch, SetStateAction } from 'react';
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+import ClassNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros(){
+interface Props{
+    filtro: number | null,
+    setFiltro: Dispatch<SetStateAction<number | null>>
+}
+export default function Filtros({filtro, setFiltro}:Props){
 
     function selecionarFiltros(opcao : IOpcao){
-
+        if (opcao.id === filtro) {
+            return setFiltro(null);
+        }
+        
+        return setFiltro(opcao.id);
     }
     return(
         <div className={styles.filtros}>
             {filtros.map(opcao => (
-                <button className={styles.filtros__filtros} key={opcao.id} onClick={() => selecionarFiltros(opcao)}>
+                <button 
+                    className={ClassNames({
+                        [styles.filtros__filtro]: true,                        
+                        [styles["filtros__filtro--ativo"]]: filtro === opcao.id
+                    })}
+                    key={opcao.id}
+                    onClick={() => selecionarFiltros(opcao)}
+                >
                     {opcao.label}
                 </button>
             ))}
