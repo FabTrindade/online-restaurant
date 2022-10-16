5503dc80d12f32503b9fc920804b5ad016f7e268
FabTrindade <fabrtrindade@gmail.com>
2022-10-16 18:53:54 -0300

    Using Link instead a tag.

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import { Link } from 'react-router-dom';
 import styles from './Menu.module.scss';
 
 
@@ -20,7 +21,7 @@ export default function Menu() {
             <ul className={styles.menu__list}>
                 {rotas.map((rota, index) => (
                     <li key={index} className={styles.menu__link}>
-                        <a href={rota.to}>{rota.label}</a>
+                        <Link to={rota.to}>{rota.label}</Link>
                     </li>
                 ))}
             </ul>
