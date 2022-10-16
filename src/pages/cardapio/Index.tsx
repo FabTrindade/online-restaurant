02266947cb2e6ba244a13236c0a88c6b21494a72
FabTrindade <fabrtrindade@gmail.com>
2022-10-16 19:09:07 -0300

    Reusing the Header

--- src/pages/cardapio/Index.tsx
+++ src/pages/cardapio/Index.tsx
@@ -5,29 +5,21 @@ import Filtros from './Filtros';
 import Ordenador from './ordenador';
 import Itens from './itens';
 
-export default function Cardapio(){
+export default function Cardapio() {
     const [busca, setBusca] = useState('');
     const [filtro, setFiltro] = useState<number | null>(null);
     const [ordenador, setOrdenador] = useState('');
 
-    return(
-        <main>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                A casa da massa preferida
-                </div>
-            </header>
-            <header>
-                <section className={styles.cardapio}>
-                    <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                    <Buscador busca={busca} setBusca={setBusca}/>
-                    <div className={styles.cardapio__filtros}>
-                        <Filtros filtro = {filtro} setFiltro={setFiltro}/>
-                        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>                        
-                    </div>
-                    <Itens busca={busca} filtro={filtro} ordenador={ordenador} />    
-                </section>
-            </header>
-        </main>
+    return (
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca} />
+            <div className={styles.cardapio__filtros}>
+                <Filtros filtro={filtro} setFiltro={setFiltro} />
+                <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+            </div>
+            <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+        </section>
+
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import Menu from 'components/menu';
 import Cardapio from 'pages/cardapio/Index';
 import Inicio from 'pages/inicio';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styles from './Routes.module.scss';
 
-export default function AppRoutes(){
-    return(
-        <Router>
-            <Menu />
-            <Routes>
-                <Route path='/' element={<Inicio />} />
-                <Route path='/cardapio' element={<Cardapio />} />
-            </Routes>
-        </Router>
+export default function AppRoutes() {
+    return (
+        <main>
+            <Router>
+                <Menu />
+                <header className={styles.header}>
+                    <div className={styles.header__text}>
+                        A casa da massa preferida
+                    </div>
+                </header>
+                <Routes>
+                    <Route path='/' element={<Inicio />} />
+                    <Route path='/cardapio' element={<Cardapio />} />
+                </Routes>
+            </Router>
+        </main>
     );
 }
