4a02fe56f4fd0c950f0f13620f8ba6e47cc446f8
FabTrindade <fabrtrindade@gmail.com>
2022-10-16 19:37:02 -0300

    Creates a standart page

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,19 @@
 import Menu from 'components/menu';
+import PaginaPadrao from 'components/paginaPadrao';
 import Cardapio from 'pages/cardapio/Index';
 import Inicio from 'pages/inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Routes.module.scss';
 
 export default function AppRoutes() {
     return (
         <main>
             <Router>
                 <Menu />
-                <header className={styles.header}>
-                    <div className={styles.header__text}>
-                        A casa da massa preferida
-                    </div>
-                </header>
                 <Routes>
-                    <Route path='/' element={<Inicio />} />
-                    <Route path='/cardapio' element={<Cardapio />} />
+                    <Route path='/' element={<PaginaPadrao/>}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>
                 </Routes>
             </Router>
         </main>
