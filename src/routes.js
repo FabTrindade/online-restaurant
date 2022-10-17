569e1bcc1d71a061cc88181c8fff9e57d52d684d
FabTrindade <fabrtrindade@gmail.com>
2022-10-16 21:58:29 -0300

    Creates NotFound page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
+import Footer from 'components/footer';
 import Menu from 'components/menu';
 import PaginaPadrao from 'components/paginaPadrao';
 import Cardapio from 'pages/cardapio/Index';
 import Inicio from 'pages/inicio';
+import NotFound from 'pages/notFound';
 import Sobre from 'pages/sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,9 +16,11 @@ export default function AppRoutes() {
                     <Route path='/' element={<PaginaPadrao/>}>
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
-                        <Route path='sobre' element={<Sobre />} />
+                        <Route path='sobre' element={<Sobre />} />                        
                     </Route>
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
+                <Footer />
             </Router>
         </main>
     );
